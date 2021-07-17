1fbc7b784d39d84cbbb3c080dc0dd9fd60c2e2ff
tatsuya <a.tatsu0117@gmail.com>
2021-07-17 20:00:59 +0900

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas odio optio facere autem magnam consequatur et modi. Cumque, labore in laudantium voluptates exercitationem ut amet dolores et vel, libero sint, vero aliquid modi sunt hic fugit dolore consectetur! Eligendi, ipsum?</p>
+    </section>
+  );
+};
+
+export default About;
